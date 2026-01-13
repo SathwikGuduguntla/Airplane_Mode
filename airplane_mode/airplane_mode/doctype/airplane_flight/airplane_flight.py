b75f158cf9c12722fddd8b78f8fdb3db4d5bbec7
SathwikGuduguntla <guduguntlasathwik@gmail.com>
2026-01-13 13:35:10 +0530

    feat: Add notifications, patches, and airplane ticket controllers

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -3,9 +3,11 @@
 
 
 
-
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 class AirplaneFlight(WebsiteGenerator):
-    def on_submit(self):
-        self.status = "Completed"
+    
+    def get_context(self, context):
+      
+        pass
