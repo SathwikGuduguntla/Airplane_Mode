c24c0641aecc29e6d703dee816ea64ef5fb5f0ab
SathwikGuduguntla <guduguntlasathwik@gmail.com>
2026-01-13 16:27:54 +0530

    style: apply pre-commit auto-fixes for linting and formatting

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -2,10 +2,10 @@
 # For license information, please see license.txt
 
 
-
-
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
+
 class AirplaneFlight(WebsiteGenerator):
-    def on_submit(self):
-        self.status = "Completed"
+	def get_context(self, context):
+		pass
